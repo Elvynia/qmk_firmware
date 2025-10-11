517834b20a8f4dd46d92d165d93e1dd32a09dffe
LV <elvynia@gmail.com>
2025-10-11 15:01:09 +0200

    Adds layer for child play, map 6-0 numbers on 1-5 with caps, emote test.

--- keyboards/monsgeek/m1/keymaps/elvynia/keymap.c
+++ keyboards/monsgeek/m1/keymaps/elvynia/keymap.c
@@ -20,7 +20,7 @@ enum layers {
     WIN_B,
     WIN_CAPS,
     WIN_FN,
-    MAC_B,
+    CHILD,
     MAC_W,
     MAC_FN
 };
@@ -33,7 +33,8 @@ enum custom_keycodes {
     LV_DDBS,
     LV_DDS,
     LV_MD = UC(0x00B7),
-    LV_FN = MO(WIN_FN)
+    LV_FN = MO(WIN_FN),
+    LV_EMOTE
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -54,6 +55,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
         case LV_AWCL:
             SEND_STRING("() => ");
             return false;
+        case LV_EMOTE:
+            send_unicode_string("¯\\_(ツ)_/¯");
+            return false;
         }
     }
     return true;
@@ -72,27 +76,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [WIN_CAPS] = LAYOUT_all(
         _______,   _______, _______, _______, _______, _______, _______, _______, _______,  KC_PWR, _______, _______, _______, _______,          KC_MUTE,
-        _______,   _______, _______, _______, _______, _______, _______, _______, _______, LV_AWOP, LV_AWCL, _______,   LV_EQ, _______,          MS_BTN1,
-        _______,   _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, LV_DDBS,          MS_BTN2,
+        _______,   KC_6,    KC_7,       KC_8,    KC_9,    KC_0, _______, _______, _______, LV_AWOP, LV_AWCL, _______,   LV_EQ, _______,          MS_BTN1,
+        _______,   KC_PGDN, KC_UP,   KC_PGUP, _______, _______, _______, _______, _______, _______, _______, _______, _______, LV_DDBS,          MS_BTN2,
         _______,   KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______, _______,   KC_NO, _______,          MS_BTN4,
         _______,     KC_NO, _______, _______, _______, _______, _______, _______, _______, _______,   LV_MD,  LV_DDS,          _______, KC_W,    MS_BTN5,
         _______,   _______, _______,                    KC_ENT,                            _______, _______, _______,          KC_A,    KC_S,       KC_D),
 
     [WIN_FN] = LAYOUT_all(
-        _______, KC_MYCM, KC_MAIL, KC_WSCH, KC_WHOM, KC_MSEL, _______, _______, _______, QK_BOOT,  KC_NUM, _______, _______, _______,           KC_MUTE,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   KC_P0, RGB_SPD, RGB_SPI, _______,           _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,  _______, KC_PSCR, _______, _______, RGB_MOD,           _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, _______, _______,   KC_NO, RGB_HUI,           _______,
+        _______, KC_MYCM, KC_MAIL, KC_WSCH, KC_WHOM, KC_MSEL, _______, _______, DF(CHILD), QK_BOOT,  KC_NUM, _______, _______, _______,         LV_EMOTE,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,   KC_P0, RGB_SPD, RGB_SPI, _______,           UC_WIN,
+        _______, _______, _______, _______, _______, _______, _______, _______, KC_INS,  _______, KC_PSCR, _______, _______, RGB_MOD,           UC_WINC,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, _______, _______,   KC_NO, RGB_HUI,           UC_LINX,
         _______,   KC_NO, _______, _______, KC_CALC, _______, _______, _______, _______, KC_MPRV, KC_MNXT, KC_EJCT,          _______,  RGB_VAI, _______,
-        _______, GU_TOGG, _______,                   _______,                            KC_MENU, _______,  KC_APP,          RGB_SAD,  RGB_VAD, RGB_SAI),
+        _______, _______, _______,                   _______,                            KC_MENU, _______,  KC_APP,          RGB_SAD,  RGB_VAD, RGB_SAI),
 
-    [MAC_B] = LAYOUT_all( /* Base */
-        KC_ESC,  KC_BRID, KC_BRIU, KC_MCTL, KC_LPAD, KC_F5,   KC_F6,   KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLD,KC_VOLU,  KC_DEL,           KC_MUTE,
-        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,KC_EQL,   KC_BSPC,          KC_HOME,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,KC_RBRC,  KC_BSLS,          KC_PGUP,
-        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,KC_NUHS,  KC_ENT,           KC_PGDN,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,                             KC_RGUI, MO(MAC_FN),KC_RCTL,        KC_LEFT, KC_DOWN, KC_RGHT),
+    [CHILD] = LAYOUT_all( /* Lily */
+        KC_ESC,     KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,        KC_EQL,   KC_DEL,   LV_EMOTE,
+        LSFT(KC_1), KC_1,       KC_2,       KC_3,       KC_4,       KC_5,       KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,        KC_EQL,   KC_BSPC,  UC(0x00D0),
+        LSFT(KC_2), LSFT(KC_Q), LSFT(KC_W), LSFT(KC_E), LSFT(KC_R),	LSFT(KC_T),	LSFT(KC_Y),	LSFT(KC_U),	LSFT(KC_I),	LSFT(KC_O),	LSFT(KC_P),	KC_LBRC,        KC_RBRC,  KC_BSLS,  UC(0x2168),
+        LSFT(KC_3), LSFT(KC_A), LSFT(KC_S), LSFT(KC_D), LSFT(KC_F),	LSFT(KC_G),	LSFT(KC_H),	LSFT(KC_J),	LSFT(KC_K),	LSFT(KC_L),	KC_SCLN,    KC_QUOT,        KC_NUHS,  KC_ENT,   UC(0x058E),
+        LSFT(KC_4), KC_NUBS,    LSFT(KC_Z), LSFT(KC_X), LSFT(KC_C),	LSFT(KC_V),	LSFT(KC_B),	LSFT(KC_N),	LSFT(KC_M),	KC_COMM,    KC_DOT,     KC_SLSH,        KC_RSFT,  KC_UP,    UC(0x30C4),
+        LSFT(KC_5), LSFT(KC_6), LSFT(KC_7),                   KC_SPC,                             KC_RGUI, MO(MAC_FN),  KC_RCTL,        KC_LEFT, KC_DOWN, KC_RGHT),
 
     [MAC_W] = LAYOUT_all( /* WASD/↑←↓→ */
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,
@@ -102,8 +106,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_W,    _______,
         _______, _______, _______,                   _______,                            _______, MO(MAC_FN), _______,          KC_A,    KC_S,    KC_D),
 
-    [MAC_FN] = LAYOUT_all( /* FN */
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,  KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL,            RGB_MOD,
+    [MAC_FN] = LAYOUT_all( /* Lily FN */
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   DF(WIN_B),  KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL,            RGB_MOD,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_SPD, RGB_SPI, _______,           _______,
         _______, _______,TG(MAC_W),_______, _______, _______, _______, _______, KC_INS,  DF(WIN_B),KC_PSCR,_______, _______, RGB_MOD,           _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, RGB_TOG, _______, _______, RGB_MOD, RGB_HUI,           _______,
@@ -117,7 +121,7 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [WIN_B] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
     [WIN_CAPS] = { ENCODER_CCW_CW(MS_WHLR, MS_WHLL) },
     [WIN_FN] = { ENCODER_CCW_CW(RGB_VAI, RGB_VAD) },
-    [MAC_B] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
+    [CHILD] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
     [MAC_W] = { ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
     [MAC_FN] = { ENCODER_CCW_CW(RGB_VAI, RGB_VAD) }
 };
